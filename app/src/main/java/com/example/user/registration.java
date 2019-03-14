e4a222b188871cfd3c24326bed31ce7ba4b7fd65
Diljith <diljithm4@gmail.com>
2019-03-14 12:14:37 +0530

    added json parser

--- app/src/main/java/com/example/user/registration.java
+++ app/src/main/java/com/example/user/registration.java
@@ -2,12 +2,13 @@ package com.example.user;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class registration extends AppCompatActivity {
 
-    EditText e1,e2,e3,e4;
+    EditText e1, e2, e3, e4;
     Button b1;
 
     @Override
@@ -15,10 +16,58 @@ public class registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        e1=(EditText)findViewById(R.id.editText3);
-        e2=(EditText)findViewById(R.id.editText13);
-        e3=(EditText)findViewById(R.id.editText21);
-        e4=(EditText)findViewById(R.id.editText22);
-        b1=(Button)findViewById(R.id.button);
+        e1 = (EditText) findViewById(R.id.editText3);
+        e2 = (EditText) findViewById(R.id.editText13);
+        e3 = (EditText) findViewById(R.id.editText21);
+        e4 = (EditText) findViewById(R.id.editText22);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = e1.getText().toString();
+                String mail = e2.getText().toString();
+                String password = e3.getText().toString();
+                String phone = e4.getText().toString();
+
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("login", name));
+                params.add(new BasicNameValuePair("login", mail));
+                params.add(new BasicNameValuePair("login", password));
+                params.add(new BasicNameValuePair("login", phone));
+
+
+                JSONObject jobj = null;
+                try {
+                    jobj = (JSONObject) jsonParser.makeHttpRequest(ur, "GET", params);
+                } catch (JSONException e1) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "e111" + e1, Toast.LENGTH_LONG).show();
+                }
+                String s = null;
+
+                try {
+                    Log.d("==============", "Emmmntered");
+                    String ss = jobj.getString("task");
+                    Log.d("Msg++++++++++++++++++++", ss);
+                    if (ss.equals("invalid")) {
+                        Log.d("**********", "invalid");
+
+                    } else {
+
+
+                        Intent i = new Intent(getApplicationContext(), committe_home.class);
+                        startActivity(i);
+
+                    }
+
+
+                } catch (Exception e) { // TODO Auto-generated catch block
+                    Log.d("**********", "" + e);
+                    Toast.makeText(getApplicationContext(), "e       " + e1, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }

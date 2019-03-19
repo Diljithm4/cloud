f423216acb06ac50743cc28233aae819c65489b7
Diljith <diljithm4@gmail.com>
2019-03-19 10:51:44 +0530

    registration almost done

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.user;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText e5,e6;
@@ -15,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     String ip="";
     SharedPreferences sh;
     JSONParser jsonParser = new JSONParser();
-    // SharedPreferences sh;
     public static String ur;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +43,61 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor ed=sh.edit();
         ed.putString("ip",ip);
         ed.commit();
+        try {
+            if(Build.VERSION.SDK_INT>9)
+            {
+                StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
         ip=sh.getString("ip","");
-        ur="http://"+ip+"/registration";
+        ur="http://"+ip+"/login";
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String name = e5.getText().toString();
-                String mail = e6.getText().toString();
+                String password = e6.getText().toString();
+
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("name", name));
+                params.add(new BasicNameValuePair("pass", password));
+
+                JSONObject jobj = null;
+                try {
+                    jobj = (JSONObject) jsonParser.makeHttpRequest(ur, "GET", params);
+                } catch (JSONException e1) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "e111" + e1, Toast.LENGTH_LONG).show();
+                }
+                String s = null;
+
+                try {
+
+                    String ss = jobj.getString("task");
+
+                    if (ss.equals("failed")) {
+
+
+                    } else {
+
+
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+
+                    }
+
+
+                } catch (Exception e) { // TODO Auto-generated catch block
+
+                    Toast.makeText(getApplicationContext(), "e       " + e, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
 
     }
-}

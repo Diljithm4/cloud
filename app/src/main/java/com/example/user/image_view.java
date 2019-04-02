0292dbb6ae8f1b0576c12dd0e1dd430baad4ac72
Diljith <diljithm4@gmail.com>
2019-04-02 11:57:30 +0530

    gallery image view started

--- app/src/main/java/com/example/user/image_view.java
+++ app/src/main/java/com/example/user/image_view.java
@@ -2,12 +2,14 @@ package com.example.user;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class image_view extends AppCompatActivity {
-
+    ListView L1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
+        L1=(ListView)findViewById(R.id.list);
     }
 }

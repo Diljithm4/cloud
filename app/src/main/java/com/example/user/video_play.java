c8a086b53690c9b69450396bbd74c699e49586b5
Diljith <diljithm4@gmail.com>
2019-04-11 12:06:34 +0530

    item click code updated

--- app/src/main/java/com/example/user/video_play.java
+++ app/src/main/java/com/example/user/video_play.java
@@ -5,6 +5,8 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -18,7 +20,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class video_play extends AppCompatActivity {
+public class video_play extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView L2;
     SharedPreferences sp;
     String url="";
@@ -30,8 +32,9 @@ public class video_play extends AppCompatActivity {
         setContentView(R.layout.activity_image_view);
         L2 = (ListView) findViewById(R.id.list);
         sp= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        L2.setOnItemClickListener(this);
         try {
-            url = "http://"+ sp.getString("ip", "") +"/image_view";
+            url = "http://"+ sp.getString("ip", "") +"/video_view";
             List<NameValuePair> params = new ArrayList<NameValuePair>();
             params.add(new BasicNameValuePair("id",sp.getString("id","")));;
 
@@ -57,5 +60,10 @@ public class video_play extends AppCompatActivity {
         {
 
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }

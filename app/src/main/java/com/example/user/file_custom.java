03a0b0ec83e5a5292a1f2b5bb1d7390301254e6d
Diljith <diljithm4@gmail.com>
2019-04-12 12:09:28 +0530

    file view updated

--- app/src/main/java/com/example/user/file_custom.java
+++ app/src/main/java/com/example/user/file_custom.java
@@ -1,13 +1,121 @@
 package com.example.user;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class file_custom extends AppCompatActivity {
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class file_custom extends BaseAdapter{
+
+    private Context context;
+
+
+    ArrayList<String> c;
+    SharedPreferences sp;
+
+
+    public file_custom(Context applicationContext,ArrayList<String> file) {
+        this.context=applicationContext;
+        //this.a=x;
+        this.c=file;
+
+    }
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_file_custom);
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return c.size();
     }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        LayoutInflater inflator=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+        if(convertView==null)
+        {
+            gridView=new View(context);
+            gridView=inflator.inflate(R.layout.activity_image_custom, null);
+
+        }
+        else
+        {
+            gridView=(View)convertView;
+
+        }
+
+
+        ImageView img=(ImageView)gridView.findViewById(R.id.imageView2);
+
+        sp=PreferenceManager.getDefaultSharedPreferences(context);
+        java.net.URL thumb_u;
+        try {
+            String url="http://"+sp.getString("ip","")+"/static/file_upload/"+c.get(position);
+
+            Toast.makeText(context, url, Toast.LENGTH_LONG).show();
+
+            thumb_u = new java.net.URL("http://"+sp.getString("ip","")+"/static/file_upload/"+c.get(position));
+            Drawable thumb_d = Drawable.createFromStream(thumb_u.openStream(), "src");
+            img.setImageDrawable(thumb_d);
+
+
+        }
+        catch(Exception e){
+
+        }
+
+//		java.net.URL thumb_u;
+//		try {
+////
+//
+//
+//			String urll="http://"+Ipsettings.ip+"//women%20security//upload//"+b[position];
+//			Picasso.with(Context)
+//		    .load(urll)
+//		    //.transform(new Circulartransform())
+//		    .error(R.drawable.c)
+//		    .into(img);
+//
+//
+//
+//		}
+//		catch(Exception e){
+//
+//		}
+//
+
+
+
+        return gridView;
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/user/file_view.java
+++ app/src/main/java/com/example/user/file_view.java
@@ -1,13 +1,61 @@
 package com.example.user;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class file_view extends AppCompatActivity {
-
+    ListView list1;
+    SharedPreferences sp;
+    String url="";
+    JSONParser parser=new JSONParser();
+    ArrayList<String> file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_view);
+        list1 = (ListView) findViewById(R.id.list);
+        sp= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        try {
+            url = "http://"+ sp.getString("ip", "") +"/file_view";
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("id",sp.getString("id","")));;
+
+            JSONArray ar = null;
+            try {
+                ar = (JSONArray) parser.makeHttpRequest(url, "GET", params);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d("11111111", ar + "");
+            file = new ArrayList<String>();
+            for (int i = 0; i < ar.length(); i++) {
+                JSONObject js = ar.getJSONObject(i);
+                file.add(js.getString("file_name"));
+                // L1.setAdapter(new Custom(getApplicationContext(),img));
+
+            }
+            list1.setAdapter(new image_custom(getApplicationContext(),file));
+
+
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 }
